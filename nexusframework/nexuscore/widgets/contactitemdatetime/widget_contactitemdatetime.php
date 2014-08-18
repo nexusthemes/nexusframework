b4db14e2f669298270c93855487210d530d536dc
Johan van Seijen <vanseijen@gmail.com>
2014-08-18 20:54:09 +0200

    update

--- nexusframework/nexuscore/widgets/contactitemdatetime/widget_contactitemdatetime.php
+++ nexusframework/nexuscore/widgets/contactitemdatetime/widget_contactitemdatetime.php
@@ -273,10 +273,8 @@ function nxs_widgets_contactitemdatetime_render_webpart_render_htmlvisualization
 	<div ' . $class . '>
 		<div class="content2">
 			<div class="box">
-	        	<div class="box-title">
-					<h4>Datetime input element: ' . $formlabel . '</h4>
-				</div>
-				<div class="box-content"></div>
+	        	<div class="box-title nxs-width20"><h4><span class="nxs-icon-clock" style="font-size: 16px;" /> Date and time</h4></div>
+				<div class="box-content nxs-width80">'.$formlabel.'</div>
 			</div>
 			<div class="nxs-clear"></div>
 		</div>
