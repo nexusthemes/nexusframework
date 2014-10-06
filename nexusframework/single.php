b4fcc922eada5ff77d901f9719246f7ccd756de8
Gert-Jan Bark <barkgj@nexusthemes.com>
2014-10-06 10:16:46 +0200

    Attachments
    * Attachments are opened in the browser rather than showing a link
    pointing to the file

--- nexusframework/single.php
+++ nexusframework/single.php
@@ -1,3 +1,17 @@
 <?php	
-	require_once(NXS_FRAMEWORKPATH . '/content.php');
+	if (is_attachment())
+	{
+		/*
+		$mimetype = get_post_mime_type();
+  	echo "mimetype; $mimetype <br />";
+		echo "attachment url: $url <br />";
+		*/
+		$url = wp_get_attachment_url();
+		wp_redirect($url, 301);
+		die();
+	}
+	else
+	{
+		require_once(NXS_FRAMEWORKPATH . '/content.php');
+	}
 ?>
