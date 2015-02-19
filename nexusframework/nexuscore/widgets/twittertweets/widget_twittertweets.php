b6b27a032f47ab17df61c32a57e1dd850373ed81
tedgodyla <tedgodyla@gmail.com>
2015-02-19 16:42:09 +0100

    Twitter
    * Twitter widget shows weird characters #27

--- nexusframework/nexuscore/widgets/twittertweets/widget_twittertweets.php
+++ nexusframework/nexuscore/widgets/twittertweets/widget_twittertweets.php
@@ -230,60 +230,60 @@ function nxs_widgets_twittertweets_twitterifyv2($tweet)
 	$user_mention_link_pattern = '<a href="http://twitter.com/%s" rel="nofollow" target="_blank" title="%s">@%s</a>';
 	$media_link_pattern = '<a href="%s" rel="nofollow" target="_blank" title="%s">%s</a>';
 	
-  $text = $tweet->text;
-  
-  $entity_holder = array();
-  
-  foreach($tweet->entities->hashtags as $hashtag)
-  {
-    $entity = new stdclass();
-    $entity->start = $hashtag->indices[0];
-    $entity->end = $hashtag->indices[1];
-    $entity->length = $hashtag->indices[1] - $hashtag->indices[0];
-    $entity->replace = sprintf($hashtag_link_pattern, strtolower($hashtag->text), $hashtag->text);
-    
-    $entity_holder[$entity->start] = $entity;
-  }
-  
-  foreach($tweet->entities->urls as $url)
-  {
-    $entity = new stdclass();
-    $entity->start = $url->indices[0];
-    $entity->end = $url->indices[1];
-    $entity->length = $url->indices[1] - $url->indices[0];
-    $entity->replace = sprintf($url_link_pattern, $url->url, $url->expanded_url, $url->display_url);
-    
-    $entity_holder[$entity->start] = $entity;
-  }
-  
-  foreach($tweet->entities->user_mentions as $user_mention)
-  {
-    $entity = new stdclass();
-    $entity->start = $user_mention->indices[0];
-    $entity->end = $user_mention->indices[1];
-    $entity->length = $user_mention->indices[1] - $user_mention->indices[0];
-    $entity->replace = sprintf($user_mention_link_pattern, strtolower($user_mention->screen_name), $user_mention->name, $user_mention->screen_name);
-    
-    $entity_holder[$entity->start] = $entity;
-  }
-  
-  foreach($tweet->entities->media as $media)
-  {
-    $entity = new stdclass();
-    $entity->start = $media->indices[0];
-    $entity->end = $media->indices[1];
-    $entity->length = $media->indices[1] - $media->indices[0];
-    $entity->replace = sprintf($media_link_pattern, $media->url, $media->expanded_url, $media->display_url);
-    
-    $entity_holder[$entity->start] = $entity;
-  }
-  
-  krsort($entity_holder);
-  foreach($entity_holder as $entity)
-  {
-    $text = substr_replace($text, $entity->replace, $entity->start, $entity->length);
-  }
-	
+	$text = $tweet->text;
+
+	$entity_holder = array();
+
+	foreach($tweet->entities->hashtags as $hashtag)
+	{
+		$entity = new stdclass();
+		$entity->start = $hashtag->indices[0];
+		$entity->end = $hashtag->indices[1];
+		$entity->length = $hashtag->indices[1] - $hashtag->indices[0];
+		$entity->replace = sprintf($hashtag_link_pattern, strtolower($hashtag->text), $hashtag->text);
+
+		$entity_holder[$entity->start] = $entity;
+	}
+
+	foreach($tweet->entities->urls as $url)
+	{
+		$entity = new stdclass();
+		$entity->start = $url->indices[0];
+		$entity->end = $url->indices[1];
+		$entity->length = $url->indices[1] - $url->indices[0];
+		$entity->replace = sprintf($url_link_pattern, $url->url, $url->expanded_url, $url->display_url);
+
+		$entity_holder[$entity->start] = $entity;
+	}
+
+	foreach($tweet->entities->user_mentions as $user_mention)
+	{
+		$entity = new stdclass();
+		$entity->start = $user_mention->indices[0];
+		$entity->end = $user_mention->indices[1];
+		$entity->length = $user_mention->indices[1] - $user_mention->indices[0];
+		$entity->replace = sprintf($user_mention_link_pattern, strtolower($user_mention->screen_name), $user_mention->name, $user_mention->screen_name);
+
+		$entity_holder[$entity->start] = $entity;
+	}
+
+	foreach($tweet->entities->media as $media)
+	{
+		$entity = new stdclass();
+		$entity->start = $media->indices[0];
+		$entity->end = $media->indices[1];
+		$entity->length = $media->indices[1] - $media->indices[0];
+		$entity->replace = sprintf($media_link_pattern, $media->url, $media->expanded_url, $media->display_url);
+
+		$entity_holder[$entity->start] = $entity;
+	}
+
+	krsort($entity_holder);
+	foreach($entity_holder as $entity)
+	{
+		$text = mb_substr($text, 0, $entity->start).$entity->replace.mb_substr($text, $entity->start+$entity->length);
+	}
+
 	return $text;
 }
 
