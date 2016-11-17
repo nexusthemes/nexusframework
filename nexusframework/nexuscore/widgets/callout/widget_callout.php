f08c9e73f659c07ad172dbbc2b2d636ed8c33cbe
Gert-Jan Bark <barkgj@nexusthemes.com>
2016-11-17 02:09:47 +0100

    Call out widget
    * Call out widget; image_src now supports lookup translations too

--- nexusframework/nexuscore/widgets/callout/widget_callout.php
+++ nexusframework/nexuscore/widgets/callout/widget_callout.php
@@ -389,7 +389,7 @@ function nxs_widgets_callout_render_webpart_render_htmlvisualization($args)
 	$mixedattributes = nxs_localization_localize($mixedattributes);
 	
 	// Lookup atts
-	$mixedattributes = nxs_filter_translatelookup($mixedattributes, array("title","subtitle", "button_text","destination_url"));	
+	$mixedattributes = nxs_filter_translatelookup($mixedattributes, array("title","subtitle", "button_text","destination_url", "image_src"));	
 	
 	// Output the result array and setting the "result" position to "OK"
 	$result = array();
