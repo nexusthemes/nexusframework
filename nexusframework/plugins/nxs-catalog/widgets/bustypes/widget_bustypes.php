abfcb27d770d8271efeef01ed2bba0e35b4cbf7b
Johan van Seijen <vanseijen@gmail.com>
2016-12-16 11:00:17 +0100

    mayor update

--- nexusframework/plugins/nxs-catalog/widgets/bustypes/widget_bustypes.php
+++ nexusframework/plugins/nxs-catalog/widgets/bustypes/widget_bustypes.php
@@ -43,7 +43,76 @@ function nxs_widgets_bustypes_home_getoptions($args)
 			"group" => nxs_widgets_bustypes_getunifiedstylinggroup(),
 		),		
 			
-		"fields" => $fields,
+		"fields" => array
+		(
+						
+			
+			// CONFIGURATION
+			
+			array( 
+				"id" 				=> "wrapper_begin",
+				"type" 				=> "wrapperbegin",
+				//"initial_toggle_state"	=> "closed",
+				"label" 			=> nxs_l18n__("Configuration", "nxs_td"),
+			),
+			
+			array( 
+				"id" 				=> "primarycolor",
+				"type" 				=> "colorzen",
+				"label" 			=> nxs_l18n__("Primary Color", "nxs_td"),
+				"unistylablefield"	=> true
+			),
+			array( 
+				"id" 				=> "defaultcolor",
+				"type" 				=> "colorzen",
+				"label" 			=> nxs_l18n__("Default Color", "nxs_td"),
+				"unistylablefield"	=> true
+			),
+			array(
+				"id" 				=> "layout",
+				"type" 				=> "select",
+				"label" 			=> nxs_l18n__("Layout", "nxs_td"),
+				"dropdown" 			=> array(
+					""		=>"default",
+					"primary"		=>"primary",
+				),
+				"unistylablefield"	=> true
+			),	
+			array(
+				"id" 				=> "numofcolumns",
+				"type" 				=> "select",
+				"label" 			=> nxs_l18n__("Columns", "nxs_td"),
+				"dropdown" 			=> array(
+					""				=>"default",
+					"2"				=>"2",
+					"3"				=>"3",
+					"4"				=>"4",
+					"6"				=>"6",
+					"8"				=>"8",
+				),
+				"unistylablefield"	=> true
+			),
+			array(
+				"id" 				=> "subnumofcolumns",
+				"type" 				=> "select",
+				"label" 			=> nxs_l18n__("Sub Columns", "nxs_td"),
+				"dropdown" 			=> array(
+					""				=>"default",
+					"2"				=>"2",
+					"3"				=>"3",
+					"4"				=>"4",
+					"6"				=>"6",
+					"8"				=>"8",
+				),
+				"unistylablefield"	=> true
+			),					
+			
+			array( 
+				"id" 				=> "wrapper_end",
+				"type" 				=> "wrapperend"
+			),
+			
+		)
 	);
 	
 	return $options;
@@ -93,18 +162,129 @@ function nxs_widgets_bustypes_render_webpart_render_htmlvisualization($args)
 	//	$alternativemessage = nxs_l18n__("Warning: no button text", "nxs_td");
 	//}
 
+
+	// https://nexusthemes.com/api/1/prod/businesstypes/?nxs=api&nxs_json_output_format=prettyprint
+	// https://nexusthemes.com/api/1/prod/primary-businesstypes/?nxs=api&fingerprint=johan&lang=en&nxs_json_output_format=prettyprint
+
+	$url = "https://nexusthemes.com/api/1/prod/primary-businesstypes/?nxs=api&fingerprint=sitemap&lang=en&nxs_json_output_format=prettyprint";
+	$primarybusinesstypesjson = file_get_contents($url);
+	$primarybusinesstypes = json_decode($primarybusinesstypesjson, true);
+	$primarybusinesstypes = $primarybusinesstypes["primary_businesstypes"];
+	
+	// Color
+	$primarycolor_cssclass = nxs_getcssclassesforlookup("nxs-colorzen-", $primarycolor);
+	$defaultcolor_cssclass = nxs_getcssclassesforlookup("nxs-colorzen-", $defaultcolor);
 	
 	// OUTPUT
+	// ----------------------------------------------------------------------------------------------------
 	// ---------------------------------------------------------------------------------------------------- 
 	
-	if ($alternativemessage != "" && $alternativemessage != null)
-	{
-		nxs_renderplaceholderwarning($alternativemessage);
-	} 
-	else 
-	{
-		echo "johan johan johan";
+	if ($numofcolumns == "") { $numofcolumns = '6'; }
+	if ($subnumofcolumns == "") { $subnumofcolumns = '8'; }
+	
+	echo '<div id="nxsgrid-container">';
+	
+	// PRIMARY version
+	// ----------------------------------------------------------------------------------------------------
+	
+	if ($layout == "primary") {
+    
+		$index = 0;
+		foreach ($primarybusinesstypes as $industry => $industrymeta) {
+			
+			// Default modulo to separate lines of items when content height is variable
+			if ($index % $numofcolumns == 0 && $index != 0) { echo '<div class="nxs-clear"></div>'; }
+			
+			// Exceptional modulo for two step separation with four column gallery
+			if ($index % 2 == 0 && $index != 0) { echo '<div class="nxs-clear multi-step-divider"></div>'; }
+		
+			$index = $index + 1;
+			$salesurl = $industrymeta["salesurl"];
+			$name = $industrymeta["name"];
+			$businesstypes = $industrymeta["businesstypes"];
+			
+			// PRIMARY BUSINESS TYPES
+			// ----------------------------------------------------------------------------------------------------
+			echo '
+			<div class="nxsgrid-item nxssolidgrid-column-'.$numofcolumns.' nxsgrid-float-left">
+			
+				<div class="nxsgrid-item-container nxs-padding10 nxs-align-center '.$primarycolor_cssclass.'">
+					<span class="nxs-icon-'.$industry.' nxs-icon-scale-1-0 nxs-margin-bottom10"></span>		
+					<h6>'.$name.'</h6>
+				</div>';
+				
+				// BUSINESS TYPES
+				// ----------------------------------------------------------------------------------------------------
+				echo'<div class="nxsgrid-subcontainer">';
+				
+					echo '
+					<div class="nxs-padding-top10 nxs-padding-left10">
+						<span class="nxs-icon-'.$industry.' nxs-icon-scale-1-0 nxs-float-left"></span>
+						<h2 class="nxs-padding-left10 nxs-inline" style="line-height: 32px;">'.$name.'</h2>
+					</div>
+					<div class="nxs-clear"></div>
+					';
+					
+				$subindex = 0;
+				foreach ($businesstypes as $businesstype => $businesstypemeta) {	
+				
+				// Default modulo to separate lines of items when content height is variable
+				if ($subindex % $subnumofcolumns == 0 && $subindex != 0) { echo '<div class="nxs-clear"></div>'; }
+				
+				// Exceptional modulo for two step separation with four column gallery
+				if ($subindex % 2 == 0 && $subindex != 0) { echo '<div class="nxs-clear multi-step-divider"></div>'; }
+				
+				$subindex = $subindex + 1;
+				$name = $businesstypemeta["name"];
+				
+					echo'
+					<div class="nxsgrid-subitem nxssolidgrid-column-'.$subnumofcolumns.' nxsgrid-float-left">
+						<div class="nxsgrid-item-container nxs-padding10  nxs-align-center '.$defaultcolor_cssclass.'">
+							<span class="nxs-icon-'.$businesstype.' nxs-icon-scale-1-0 nxs-margin-bottom10"></span>
+							<h6>'.$name.'</h6>
+						</div>
+					</div>';
+				}
+				echo'</div> <!-- END nxsgrid-subcontainer -->';
+			
+			echo '</div> <!-- END nxsgrid-item -->';
+		
+		}
+	
+	// NON-PRIMARY version
+	// ----------------------------------------------------------------------------------------------------
+
+	} else if ($layout == "default" || $layout == "") {
+	
+		foreach ($primarybusinesstypes as $industry => $industrymeta) {
+			
+			$salesurl = $industrymeta["salesurl"];
+			$name = $industrymeta["name"];
+			$businesstypes = $industrymeta["businesstypes"];
+			
+			foreach ($businesstypes as $businesstype => $businesstypemeta) {
+				
+				$name = $businesstypemeta["name"];
+				
+				// BUSINESS TYPES
+				// ----------------------------------------------------------------------------------------------------
+				echo '
+				<div class="nxsgrid-item nxssolidgrid-column-'.$numofcolumns.' nxsgrid-float-left">
+					
+					<div class="nxsgrid-item-container nxs-padding10  nxs-align-center '.$primarycolor_cssclass.'">
+						<span class="nxs-icon-'.$businesstype.' nxs-icon-scale-1-0 nxs-margin-bottom10"></span>		
+						<h6 class="nxs-heightiq nxs-heightiq-p1-title">'.$name.'</h6>
+					</div>';
+					
+				echo '</div> <!-- END nxsgrid-item -->';
+			}
+		
+		}
+	
 	}
+	
+	
+	echo '</div> <!-- END nxsgrid-container -->';
 
 	// note, we set the generic widget hover menu AFTER rendering, as the blog widget
 	// will also set the generic hover menu; we don't want to see the generic hover
