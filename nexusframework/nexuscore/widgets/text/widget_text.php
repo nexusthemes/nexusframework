534961d05f5937e0c5ec07cada060351eec8514b
Gert-Jan Bark <barkgj@nexusthemes.com>
2018-01-12 15:16:22 +0100

    Text Widget
    * Text Widget; the lookups block is closed by default, unless it
    contains content

--- nexusframework/nexuscore/widgets/text/widget_text.php
+++ nexusframework/nexuscore/widgets/text/widget_text.php
@@ -50,7 +50,8 @@ function nxs_widgets_text_home_getoptions($args)
 				"id" 				=> "wrapper_title_begin",
 				"type" 				=> "wrapperbegin",
 				"label" 			=> nxs_l18n__("Lookups", "nxs_td"),
-				"initial_toggle_state" => "closed",
+				"initial_toggle_state"	=> "closed-if-empty",
+				"initial_toggle_state_id" => "lookups",
 			),
 			array
       (
