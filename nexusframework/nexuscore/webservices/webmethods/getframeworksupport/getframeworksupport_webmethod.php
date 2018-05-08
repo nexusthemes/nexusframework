afbfb447947f6a50c49f539281937696c1674681
Gert-Jan Bark <barkgj@nexusthemes.com>
2018-05-08 19:36:37 +0200

    GDPR integrated support
    * GDPR integrated support; the integrated support sends the question to
    our server, and therefore can only be used if there is a consent
    (requires a valid license; the license is only provided when there's
    consent when the purchase was made and when the license was activated).

--- nexusframework/nexuscore/webservices/webmethods/getframeworksupport/getframeworksupport_webmethod.php
+++ nexusframework/nexuscore/webservices/webmethods/getframeworksupport/getframeworksupport_webmethod.php
@@ -6,28 +6,54 @@
 		
 		header('HTTP/1.0 200 OK');
 		
-		// no data, or expired data
 		$licensekey = get_option('nxs_licensekey');
-		$serviceparams = array
-		(
-			'timeout' => 15,
-			'sslverify' => false,
-			'body' => array
-			(
-				"q" => $q,
-				"action" => "nxs_ajax_webmethods",
-				"webmethod" => "getsupportfeedback",
-				"licensekey" => $licensekey,
-				"type" => "framework",
-			)
-		);
 		
-		$url = "http://support.nexusthemes.com/";
-		$response = wp_remote_post($url, $serviceparams);
-		$body = wp_remote_retrieve_body($response);
-	
-		echo $body;
-		die();
+		if ($licensekey == "")
+		{
+			// we are sure the license is not valid; don't attempt to query the server
+			
+			// 
+			?>
+			{
+			  "hints": [
+			    {
+			      "title": "The support section requires a valid Nexus license. Your site is currently not connected to a valid Nexus license. <a target='_blank' href='https://www.wpsupporthelp.com/answer/how-to-register-your-wordpress-theme-purchase-to-get-updates-1091/'>Learn more</a>",
+			      "type": "text",
+			      "youtubeid": null,
+			      "thumbimgurl": null,
+			      "meta": ""
+			    }
+			  ],
+			  "autolivechat": "show",
+			  "result": "OK"
+			}
+			<?php
+			die();
+		}
+		else
+		{
+			// the licensekey is set; it could be valid, but it could also not be valid
+						
+			$serviceparams = array
+			(
+				'timeout' => 15,
+				'sslverify' => false,
+				'body' => array
+				(
+					"q" => $q,
+					"action" => "nxs_ajax_webmethods",
+					"webmethod" => "getsupportfeedback",
+					"licensekey" => $licensekey,
+					"type" => "framework",
+				)
+			);
+			
+			$url = "https://support1805.nexusthemes.com/";
+			$response = wp_remote_post($url, $serviceparams);
+			$body = wp_remote_retrieve_body($response);
+		
+			echo $body;
+			die();
+		}
 	}
-	
 ?>
